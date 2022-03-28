ecc39709c5964dfcea920c855a0663e374cc527e
Adam Puchała <adam.puchala@outlook.com>
2022-03-28 18:06:23 +0200

    added typescript jest unit tests

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
+npm run test
 npx tsc -b
+cargo test
 wasm-pack build --target web --out-dir ./static/script/wasm-module
 rm ./static/script/wasm-module/package.json
 mkdir -p ./src/typescript/wasm-module
