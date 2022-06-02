773f05e5a38d80b525d6991acd260014684eb9a8
Adam Puchała <adam.puchala@outlook.com>
2022-06-02 15:36:06 +0200

    use benchmark functions from webassembly

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -23,7 +23,7 @@ function tsMain() {
 import('./../../pkg').then((module)=> {
     console.log(`webassembly ${module.run_wasm_benchmarks()}`)
     console.log(`webassembly polybench 3mm ${module.launch_3mm_polybench_benchmark()}`)
-    // console.log(`webassembly polybench covariance ${module.launch_covariance_polybench_benchmark()}`)
+    console.log(`webassembly polybench covariance ${module.launch_covariance_polybench_benchmark()}`)
     console.log(`webassembly polybench syr2k ${module.launch_syr2k_polybench_benchmark()}`)
     console.log(`webassembly polybench ludcmp ${module.launch_ludcmp_polybench_benchmark()}`)
     console.log(`webassembly polybench floyd warshall ${module.launch_floyd_warshall_polybench_benchmark()}`)
@@ -34,5 +34,5 @@ import('./../../pkg').then((module)=> {
     console.log(`javascript polybench syr2k ${benchSyr2k()}`)
     console.log(`javascript polybench ludcmp ${benchLudCmp()}`)
     console.log(`javascript polybench floydWarshall ${benchFloydWarshall()}`)
-    // console.log(`javascript polybench heat3D ${benchHeat3D()}`)
+    console.log(`javascript polybench heat3D ${benchHeat3D()}`)
 })
